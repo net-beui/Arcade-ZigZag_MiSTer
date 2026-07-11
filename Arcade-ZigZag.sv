0465a6cdedad20574de0bc365d3e88728262554a
sbugliarisi <42358150+net-beui@users.noreply.github.com>
2026-07-10 22:15:31 -0700

    Update SYS and add RBF
    Update SYS and add RBF

--- Arcade-ZigZag.sv
+++ Arcade-ZigZag.sv
@@ -28,7 +28,7 @@ module emu
 	input         RESET,
 
 	//Must be passed to hps_io module
-	inout  [48:0] HPS_BUS,
+	inout  [45:0] HPS_BUS,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        CLK_VIDEO,
@@ -56,6 +56,8 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output 		  HDMI_BLACKOUT,
+	output 		  HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -173,6 +175,8 @@ module emu
 	input         OSD_STATUS
 );
 
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 assign VGA_F1    = 0;
 assign VGA_SCALER= 0;
 assign VGA_DISABLE = 0;
